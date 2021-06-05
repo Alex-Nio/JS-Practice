aaf59b6ae774987086201be2f00c2b15182c4962
Alex <volpenio@gmail.com>
2021-06-05 18:00:18 +0300

    js practice lesson 12,13 update,lesson 14 begin

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -190,35 +190,35 @@ let ifChecked2 = false,
 
 // 1 Задание:
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
 
 
-// 2 Задание:
+// // 2 Задание:
 
-let myArr = [],
-	 emptyObj = {},
-	 emptyObj2 = {};
+// let myArr = [],
+// 	 emptyObj = {},
+// 	 emptyObj2 = {};
 
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: emptyObj,
-	actors: emptyObj2,
-	genres: myArr,
-	privat: false
-}
+// const personalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: emptyObj,
+// 	actors: emptyObj2,
+// 	genres: myArr,
+// 	privat: false
+// }
 
-// 3 Задание:
+// // 3 Задание:
 
-const ask1 = prompt('Один из последних просмотренных фильмов?', ''),
-		ask2 = prompt('На сколько оцените его?', ''),
-		ask3 = prompt('Один из последних просмотренных фильмов?', ''),
-		ask4 = prompt('На сколько оцените его?', '');
+// const ask1 = prompt('Один из последних просмотренных фильмов?', ''),
+// 		ask2 = prompt('На сколько оцените его?', ''),
+// 		ask3 = prompt('Один из последних просмотренных фильмов?', ''),
+// 		ask4 = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[ask1] = ask2;
-personalMovieDB.movies[ask3] = ask4;
+// personalMovieDB.movies[ask1] = ask2;
+// personalMovieDB.movies[ask3] = ask4;
 
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
 // -----------------------------------------------
 // -----------------------------------------------
@@ -253,6 +253,101 @@ switch(num) {
 		break;
 }
 
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// Циклы.Урок 12.
+
+let number = 1;
+
+// while(number < 10) {
+// 	console.log(number);
+// 	number++;
+// }
+
+// do {
+// 	console.log(number);
+// 	number++;
+// }
+
+// while (number < 10);
+
+// for (let i = 1; i < 8; i++) { // Цикл.
+// 	if (i === 6) { // Условие.
+// 		break; // Прерывание цикла досрочно
+// 	}
+
+// 	console.log(i);
+// }
+
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// Урок 13.Практика.
+
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+// Решение.
+
+// 1 Задание:
+
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+
+// let myArr = [],
+// 	 emptyObj = {},
+// 	 emptyObj2 = {};
+
+// const personalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: emptyObj,
+// 	actors: emptyObj2,
+// 	genres: myArr,
+// 	privat: false
+// }
+
+// // Решение.
+
+// for (let i = 0; i < 2; i++) { // Цикл.
+// 	const ask1 = prompt('Один из последних просмотренных фильмов?', ''),
+// 			ask2 = prompt('На сколько оцените его?', '');
+
+// 	if(ask1 != null && ask2 != null && ask1 != '' && ask2 != '' && ask1.length < 50) {
+// 		personalMovieDB.movies[ask1] = ask2;
+// 		console.log('done!');
+// 	} else {
+// 		console.log('error -___-');
+// 		i--;
+// 	}
+// }
+
+// if (personalMovieDB.count < 10) {
+// 	alert("Просмотрено довольно мало фильмов");
+// } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+// 	alert("Вы классический зритель");
+// } else if (personalMovieDB.count >= 30) {
+// 	console.log("Вы киноман!");
+// } else {
+// 	alert("Error!!!");
+// }
+
+// console.log(personalMovieDB);
+
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// Урок 14.Функции.
 
 // -----------------------------------------------
 // Функционал Страницы
