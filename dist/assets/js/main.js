f0d21bdc85b5ab92982d188b7fb7693af680a87e
Alex <volpenio@gmail.com>
2021-06-06 18:18:21 +0300

    add Lesson 22.Lesson 23 begin

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -866,6 +866,135 @@ console.log('=========================================');
 console.log('=*=*=*=*=*=*=*==Lesson 21==*=*=*=*=*=*=*=');
 console.log('=========================================');
 
+const soldier = {
+	health: 500,
+	armor: 100,
+	sayHello: function() {
+		console.log('Hello!');
+	}
+};
+
+// const john = {
+// 	health: 100
+// };
+
+// Устанавливаем прототип объекта
+const john = Object.create(soldier);
+
+// Получаем прототип объекта джона от солдата
+// Object.setPrototypeOf(john, soldier);
+
+john.sayHello();
+
+
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// Урок 22.Практика.
+
+console.log('=========================================');
+console.log('=*=*=*=*=*=*=*==Lesson 22==*=*=*=*=*=*=*=');
+console.log('=========================================');
+
+/* Задание на урок:
+
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+
+// Решение.
+
+// let myArr = [],
+// 	 emptyObj = {},
+// 	 emptyObj2 = {};
+
+// const personalMovieDB = {
+// 	count: 0,
+// 	movies: emptyObj,
+// 	actors: emptyObj2,
+// 	genres: myArr,
+// 	privat: false,
+// 	start: function(){
+// 		personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", '');
+
+// 		while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+// 			personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", '');
+// 		}
+// 	},
+// 	rememberMyFilms: function(){
+// 		for (let i = 0; i < 2; i++) { // Цикл.
+// 			const ask1 = prompt('Один из последних просмотренных фильмов?', ''),
+// 					ask2 = prompt('На сколько оцените его?', '');
+
+// 			if(ask1 != null && ask2 != null && ask1 != '' && ask2 != '' && ask1.length < 50) {
+// 				personalMovieDB.movies[ask1] = ask2;
+// 				console.log('done!');
+// 			} else {
+// 				console.log('error -___-');
+// 				i--;
+// 			}
+// 		}
+// 	},
+// 	detectPersonalLevel: function(){
+// 		if (personalMovieDB.count < 10) {
+// 			alert("Просмотрено довольно мало фильмов");
+// 		} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+// 			alert("Вы классический зритель");
+// 		} else if (personalMovieDB.count >= 30) {
+// 			console.log("Вы киноман!");
+// 		} else {
+// 			alert("Error!!!");
+// 		}
+// 	},
+// 	showMyDB: function(){
+// 		if(personalMovieDB.privat == false) {
+// 		console.log(personalMovieDB);
+// 		} else {
+
+// 		}
+// 	},
+// 	toggleVisibleMyDB: function(){
+// 		if(personalMovieDB.privat == false) {
+// 			personalMovieDB.privat == true;
+// 		} else {
+// 			personalMovieDB.privat == false;
+// 		}
+// 	},
+// 	writeYourGenres: function(){
+// 		for (var i = 1; i <= 3; i++) {
+// 			let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+			
+// 			if(genre === '' || genre == null){
+// 				console.log('Вы ввели некорректные данные или не ввели их вообще');
+// 				i--;
+// 			} else {
+// 				personalMovieDB.genres[i - 1] = genre;
+// 			}
+// 		}
+
+// 		personalMovieDB.genres.forEach((item, i) => {
+// 			console.log(`Любимый жанр ${i + 1} - это ${item}`);
+// 		});
+// 	}
+// };
+
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// Урок 23.Практика.
+
+console.log('=========================================');
+console.log('=*=*=*=*=*=*=*==Lesson 23==*=*=*=*=*=*=*=');
+console.log('=========================================');
+
 
 // -----------------------------------------------
 // Функционал Страницы
